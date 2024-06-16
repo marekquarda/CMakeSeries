cd07d52a29cd3ed305e4188c41533590d1e425f2
marekquarda <mquarda@centrum.cz>
2024-06-16 18:03:04 +0200

    othe changes

--- EP006/src/main.cpp
+++ EP006/src/main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 
-    std::vector<int> v = {5, 3, 5, 6};
+    std::vector<int> v = {50, 36, 5, 6};
 
     int avg = mean(v.data(), v.size());
     std::cout << "mean: " << avg << std::endl;
--- EP006/src/maths/maths.cmake
+++
@@ -1,3 +0,0 @@
-# The math library
-add_library(libmath STATIC src/maths/supermath.cpp)        
-target_include_directories(libmath PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src/maths/include)
