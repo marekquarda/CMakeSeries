e4172a6368a520a667dc847fc64147b286d29ac2
marekquarda <mquarda@centrum.cz>
2024-06-24 01:41:41 +0200

    something

---
+++ EP009/cmake/AddFmt.cmake
@@ -0,0 +1,9 @@
+# Fetch Content
+
+include(FetchContent)
+
+FetchContent_Declare(
+    fmt 
+    GIT_REPOSITORY https://github.com/fmtlib/fmt.git 
+    GIT_TAG 10.1.1
+)
--- EP009/src/main.cpp
+++ EP009/src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include "stats.h"
+#include <fmt/format.h>
 
 int main() {
 
     std::vector<int> v = {50, 36, 5, 6};
 
     int avg = mean(v.data(), v.size());
-    std::cout << "mean: " << avg << std::endl;
+    std::cout << fmt::format("means: {}", avg) << std::endl;
 
     return 0;
 }
